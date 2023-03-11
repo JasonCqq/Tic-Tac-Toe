d6ec20f46604c289a2ffb75e83c54d5a86bb4870
Jason <121495300+jason21715@users.noreply.github.com>
2023-03-11 05:43:23 -0800

    Add start screen

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// class gameBoard {
-//   constructor(board) {
-//     this.board = board;
-//   }
-// }
-// const currentBoard = new gameBoard([]);
-
-// function displayBoard() {
-//   //Displays each array into the correct divs. But I'm not too comfortable with this code.
-//   const elements = [].slice.call(document.querySelectorAll("[class^=gameBox]"));
-//   elements.forEach(function (div, idx) {
-//     if (currentBoard.board.length >= idx) {
-//       div.textContent = currentBoard.board[idx];
-//     }
-//   });
-// }
-
-//Factory function to store player values
+//Factory function to store player data
 function createPlayer(name, symbol, playerBoard) {
   return {
     name,
@@ -23,16 +6,14 @@ function createPlayer(name, symbol, playerBoard) {
     playerBoard,
 
     play(box) {
-      // currentBoard.board.push();
       playerBoard.push(box);
-      console.log(playerBoard);
     },
   };
 }
-
 const player1 = createPlayer("player1", "O", []);
 const player2 = createPlayer("player2", "X", []);
 
+//Switch symbols based on odd or even "n" number.
 let turnCheck = (function (n) {
   return function () {
     n++;
@@ -40,10 +21,10 @@ let turnCheck = (function (n) {
   };
 })(0);
 
-//Function to make divs clickable and store player's action at the same time
+//Function to make divs clickable and tie to DOM. Also to store player's action at the same time
 function playerAction() {
   const box = document.querySelectorAll(".box");
-  let turnCheckRef = turnCheck(-1);
+  let turnCheckRef = turnCheck(-1); //Reference to turnCheck fuction
   for (const boxes of box) {
     boxes.addEventListener("click", () => {
       if (boxes.textContent === "O" || boxes.textContent === "X") {
@@ -59,14 +40,38 @@ function playerAction() {
           turnCheckRef++;
         }
       }
-      //Logic to check winner each time a box is clicked.
+      //Winning/Tie Logic to check winner each time a box is clicked.
       if (player1.playerBoard.length + player2.playerBoard.length === 9) {
         console.log("TIE");
       } else {
-        switch (
-          player1.playerBoard.sort().join("") &&
-          player2.playerBoard.sort().join("")
-        ) {
+        //To check both player's board
+        switch (player1.playerBoard.sort().join("")) {
+          case "012":
+            console.log("WIN");
+            break;
+          case "345":
+            console.log("WIN");
+            break;
+          case "678":
+            console.log("WIN");
+            break;
+          case "036":
+            console.log("WIN");
+            break;
+          case "147":
+            console.log("WIN");
+            break;
+          case "258":
+            console.log("WIN");
+            break;
+          case "048":
+            console.log("WIN");
+            break;
+          case "246":
+            console.log("WIN");
+            break;
+        }
+        switch (player2.playerBoard.sort().join("")) {
           case "012":
             console.log("WIN");
             break;
@@ -93,12 +98,8 @@ function playerAction() {
             break;
         }
       }
-      console.log(player1.playerBoard);
-      console.log(player2.playerBoard);
-      console.log(turnCheckRef);
     });
   }
 }
 
-// displayBoard();
 playerAction();
